567c41eee5a4e30fca5b3525738f4b859bf6c800
N-Sirun <88889025+N-Sirun@users.noreply.github.com>
2024-01-06 22:39:26 +0400

    first commit

---
+++ Untitled-1.sql
@@ -0,0 +1,55 @@
+/*
+SELECT
+FirstName,
+LastName,
+BirthDate,
+substr(BirthDate, 1, 10),
+strftime ('%Y-%m-%d',BirthDate) as [Birth_date],
+strftime('%Y-%m-%d','now') - strftime('%Y-%m-%d',BirthDate) as [Age],
+FROM
+Employee
+
+
+SELECT
+sum(Total) as [Total Sales],
+round(avg(Total)),
+max(Total),
+min(Total),
+count(*)
+FROM
+Invoice
+
+
+SELECT
+BillingCountry,
+BillingCity,
+round(AVG(total),2)
+FROM
+Invoice
+GROUP BY
+BillingCountry, BillingCity
+ORDER BY
+BillingCountry
+*
+SELECT
+BillingAddress,
+BillingCountry,
+BillingCity,
+total
+FROM
+Invoice
+WHERE 
+total >
+(SELECT avg(total) from Invoice)
+ORDER BY 
+total
+*/
+
+SELECT 
+BillingCity,
+round(AVG(total),2),
+(SELECT round(AVG(total),2) from Invoice) as 'Global sale'
+FROM
+Invoice
+GROUP BY BillingCity
+Order BY BillingCity
